5c39f8a906961fc7ee7f24f25324b5e1457b7e5d
Nicolas MURE <nicolas.mure@knplabs.com>
2018-10-10 09:01:58 +0200

    simplify redux store creation

--- src/Redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { uncurryN } from 'ramda'
-import { createStore } from 'redux'
-
-// Store :: State -> (Store -> Store) -> (Action -> State -> State) -> Store
-export const Store = uncurryN(3,
-  initialState => middleware => reducer =>
-    createStore(reducer, initialState, middleware)
-)
