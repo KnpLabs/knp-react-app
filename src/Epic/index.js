6d9fe60de5419d2523ea7cb4dc0455213694a2cb
David Jegat <david.jegat@knplabs.com>
2019-03-08 15:04:39 +0100

    Remove source directory (start fresh)

--- src/Epic/index.js
+++
@@ -1,7 +0,0 @@
-import Dog from './Dog'
-import { combineEpics } from 'redux-observable'
-
-// Epic :: (Observable Action, Observable State) -> Observable Action
-export default combineEpics(
-  Dog,
-)
--- src/Redux/State/index.spec.js
+++
@@ -1,9 +0,0 @@
-import * as Module from './index'
-
-describe('Redux :: Module', () => {
-  it('contains the Dog state', () => {
-    const state = Module.default();
-
-    expect(state.Dog).toBeDefined();
-  });
-})
