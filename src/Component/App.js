3f67e11950619b98810629680bef02d22474bd74
David Jegat <david.jegat@knplabs.com>
2019-03-09 13:59:47 +0100

    Design a standalone homepage

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import styled from 'styled-components'
+import logo from './Asset/logo.png'
 
 
 export default () => 
-  <div>
-    <Title>Welcome</Title>
-    <SubTitle>
-      KNP React app is a full packaged solution for building
-      functional and reactive frontend application with React.
-      Bellow are a list of usefull resources to start over:
-    </SubTitle>
+  <Fragment>
+    <BlackStrip>
+      <Title>Welcome</Title>
+
+      <SubTitle>
+        KNP React app is a full packaged solution for building
+        functional and reactive frontend application with React.
+      </SubTitle>
+
+      <QuickAccess>
+        <Thumbnail href="https://github.com/KnpLabs/knp-react-app" target="_blank">
+          <Icon src="https://maxcdn.icons8.com/Share/icon/p1em/Logos/github1600.png" />
+          Take a look at the source code
+        </Thumbnail>
+
+        <Thumbnail href="Learn" target="_blank">
+          <Icon src="https://image.flaticon.com/icons/svg/171/171322.svg" />
+          Some links that might help you
+        </Thumbnail>
+
+        <Thumbnail href="Learn" target="_blank">
+          <Icon src="https://www.shareicon.net/download/2015/12/06/683054_sign.svg" />
+          Our best practices and guidelines
+        </Thumbnail>
+
+        <Thumbnail href="Learn" target="_blank">
+          <Icon src="http://chittagongit.com//images/learn-icon/learn-icon-23.jpg" />
+          See a concrete FRP application
+        </Thumbnail>
+      </QuickAccess>
+    </BlackStrip>
+
+    <Headline>What is inside ?</Headline>
+
     <Menu>
       <Item>
         <Label
@@ -106,23 +134,85 @@ export default () =>
         any impure operations and use the power of Categories to map, chain, ap ... any effects
       </Item>
     </Menu>
-  </div>
+  </Fragment>
+
+
+export const BlackStrip = styled.div`
+  max-width: 100vw;
+  box-sizing: border-box;
+  background-color: #3A3A3A;
+  display: flex;
+  flex-direction: column;
+  background-image: url(${props => logo});
+  background-repeat: no-repeat;
+  background-position: center;
+  box-shadow: 0px 0px 10px #3A3A3A;
+`
 
 
 export const Title = styled.h1`
   text-align: center;
-  color: #737373;
+  color: #DCDCDC;
   font-size: 3rem;
+`
+
+
+export const Headline = styled.h2`
+  text-align: center;
+  color: #737373;
+  font-size: 2.5rem;
 `
 
 
 export const SubTitle = styled.p`
   text-align: center;
-  color: #737373;
+  color: #DCDCDC;
   font-size: 1.2rem;
   margin: auto;
   max-width: 700px;
   opacity: .6;
+`
+
+
+export const QuickAccess = styled.div`
+  display: flex;
+  max-width: 700px;
+  text-align: center;
+  margin: 2.5rem auto;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+
+export const Thumbnail = styled.a`
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  text-decoration: none;
+  color: #DCDCDC;
+  text-align: center;
+  opacity: .6;
+  transition: all .5s;
+  border-radius: 5px;
+  padding: 1rem;
+  box-shadow: 0px 0px 1px #DCDCDC;
+  background-color: #3A3A3A;
+  &:hover {
+    opacity: 1;
+    color: #3A3A3A;
+    box-shadow: 0px 0px 5px #DCDCDC;
+    background-color: #DCDCDC;
+  }
+  margin: 1rem;
+`
+
+
+export const Icon = styled.img`
+  display: block;
+  width: 100px;
+  height: 100px;
+  margin-bottom: .5rem;
+  margin: auto;
 `
 
 
@@ -136,7 +226,6 @@ export const Menu = styled.ul`
 export const Item = styled.li`
   color: #737373;
   text-align: center;
-  border-top: 1px solid #DDDDDD;
   padding-top: 1rem;
   margin-bottom: 1.5rem;
   opacity: .6;
